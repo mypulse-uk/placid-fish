27ea6bb43e9d72c7288243f125efade585cc7137
Jordan Robinson <me@jordanrobinson.co.uk>
2023-07-04 14:34:35 +0100

    More CI fixes

--- pipelines/publish-library/task.sh
+++ pipelines/publish-library/task.sh
@@ -8,8 +8,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_DIR="$( cd "$SCRIPT_DIR/../../.." && pwd )"
 
 cd "$PROJECT_DIR"
+cd source
 
-git pull
+ls
+pwd
 
 mkdir -p ~/.lein
 
