7aec486b8db27fb0e4f2cb5183e68fd4758c509d
Jordan Robinson <me@jordanrobinson.co.uk>
2023-07-17 09:18:50 +0100

    Makes release as simple as possible for now

--- project.clj
+++ project.clj
@@ -39,22 +39,7 @@
      ["deploy"]]}
    :release
    {:release-tasks
-    [["shell" "git" "diff" "--exit-code"]
-     ["change" "version" "leiningen.release/bump-version" "release"]
-     ["codox"]
-     ["changelog" "release"]
-     ["shell" "sed" "-E" "-i.bak" "s/\"[0-9]+\\.[0-9]+\\.[0-9]+\"/\"${:version}\"/g" "README.md"]
-     ["shell" "rm" "-f" "README.md.bak"]
-     ["shell" "git" "add" "."]
-     ["vcs" "commit" "Release version %s [skip ci]"]
-     ["vcs" "tag"]
-     ["deploy"]
-     ["change" "version" "leiningen.release/bump-version" "patch"]
-     ["change" "version" "leiningen.release/bump-version" "rc"]
-     ["change" "version" "leiningen.release/bump-version" "release"]
-     ["vcs" "commit" "Pre-release version %s [skip ci]"]
-     ["vcs" "tag"]
-     ["vcs" "push"]]}}
+    [["deploy"]]}}
 
   :target-path "target/%s/"
 
