c1360067f9944b4da74dd83122f761d848daa88f
Jordan Robinson <me@jordanrobinson.co.uk>
2023-07-21 14:48:01 +0100

    Typo fix

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   (or (System/getenv "VERSION")
       "2.0.0"))
 
-(defproject ai.mypulse/placid-fish "2.0.0"
+(defproject ai.mypulse/placid-fish version
   :description "Safe url assertions for test classes that uses Exploding Fish under the hood"
   :url "https://github.com/Global-Online-Health/placid-fish"
 
